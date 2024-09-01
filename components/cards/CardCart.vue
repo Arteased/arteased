5368b8aa49270decfd0a1b22468d175bfcc3950a
Arteased <cs.arteased@gmail.com>
2024-09-01 23:48:22 +0700

    forth upload

--- components/cards/CardCart.vue
+++ components/cards/CardCart.vue
@@ -1,12 +1,21 @@
+<script lang="ts" setup>
+const props = defineProps({
+  product: { 
+    type: 
+    Object, 
+    default: {} },
+});
+</script>
+
 <template>
     <section>
-        <img src=/>
+    
+        <img :src="props.product.image" />
 
-
-        <h5>Men's Streetwear Fashion</h5>
-        <p>$30</p>
-
-        <img src="" alt="">delete icon
+        <h5>{{ props.product.name }}</h5>
+        <p>Rp {{ props.product.price }}</p>
+            
+        <img src="" @click="$emit('removeCart', props.product.id)">delete icon
 
     </section>
-  </template>
+</template>
--- pages/cart.vue
+++ pages/cart.vue
@@ -1,43 +1,56 @@
+<script lang="ts" setup>
+import type { Products } from "~/types/product";
+
+const products = ref<Products[]>([]);
+const totalPrice = computed(() => {
+  return products.value
+    .filter((product) => product.price !== undefined)
+    .reduce(
+      (accumulator, currentValue) => accumulator + currentValue.price!,
+      0
+    );
+});
+
+onMounted(() => {
+  let localStorageData = localStorage.getItem("products");
+  if (localStorageData) {
+    products.value = JSON.parse(localStorageData);
+  }
+});
+
+const removeCart = (id: number) => {
+  products.value = products.value.filter((item) => item.id !== id); localStorage.setItem("products", JSON.stringify(products.value));
+}
+</script>
+
 <template>
   <section>
+
     <div>
-      <div>
-
-        <div>
-
-          <div>
-            <h1>Shopping Cart</h1>
-            <p>0 Items</p>
-          </div>
-
-          <div>
-            <CardsCardCart />
-          </div>
-
-        </div>
-
-        <div>
-          <h3>Order Summary</h3>
-          <div>
-            <div>
-
-              <div>
-                <span>Men's Streetwear Fashion</span>
-                <span>$30</span>
-              </div>
-
-            </div>
-          </div>
-
-          <div>
-            <span>Total</span>
-            <span>$30</span>
-          </div>
-
-          <button>Checkout</button>
-
-        </div>
-      </div>
+      <h1>Shopping Cart</h1>
+      <p>{{ products.length }} Items</p>
     </div>
+
+    <div v-if="products.length > 0">
+      <template v-for="(item, index) in products" :key="index">
+        <CardsCardCart :product="item" @removeCart="removeCart"/>
+      </template>
+    </div>
+
+    <div v-else>
+      <h5>Cart is empty</h5>
+    </div>
+
+    <h3>Order Summary</h3>
+    <span>{{ item.name }}</span>
+    <span>Rp {{ item.price }}</span>
+
+    <p>There are no to orders yet</p>
+
+    <span>Total</span>
+    <span>Rp {{ totalPrice }}</span>
+
+    <button>Checkout</button>
   </section>
+
 </template>
