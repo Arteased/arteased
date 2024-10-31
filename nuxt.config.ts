a492f6596c74083c879fb29dfcdc954adcac967f
Arteased <cs.arteased@gmail.com>
2024-10-31 19:29:17 +0700

    ninth+ sync

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
+    }
+  },
+  
   modules: ["@nuxtjs/tailwindcss", "@vite-pwa/nuxt", "@nuxtjs/supabase"],
   devtools: { enabled: true },
   ssr: true,
